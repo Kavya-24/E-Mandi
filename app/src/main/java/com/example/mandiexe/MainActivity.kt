cfa2c79c5e79908cf54650310642b8f4ba4895b8
Kavya-24 <Kavya_1901cs30@iitp.ac.in>
2020-12-19 17:55:42 +0530

    1. Add app update utils

--- app/src/main/java/com/example/mandiexe/MainActivity.kt
+++ app/src/main/java/com/example/mandiexe/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.mandiexe
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.Menu
+import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -11,10 +16,20 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallState
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.InstallStatus
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+    //App Update
+    private var appUpdateManager: AppUpdateManager? = null
+    private var RC_APP_UPDATE = 1249
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,4 +62,71 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+    //App Update
+    private fun updateApp() {
+
+        appUpdateManager = AppUpdateManagerFactory.create(this)
+        installStateUpdatedListener?.let { appUpdateManager!!.registerListener(it) }
+
+    }
+
+    //Install State Listener
+    var installStateUpdatedListener: InstallStateUpdatedListener? =
+        object : InstallStateUpdatedListener {
+            override fun onStateUpdate(state: InstallState) {
+                when {
+                    state.installStatus() === InstallStatus.DOWNLOADED -> {
+                        //CHECK THIS if AppUpdateType.FLEXIBLE, otherwise you can skip
+                        popupSnackbarForCompleteUpdate()
+                    }
+                    state.installStatus() === InstallStatus.INSTALLED -> {
+                        appUpdateManager?.unregisterListener(this)
+                    }
+                    else -> {
+                        Log.i(
+                            "Activity",
+                            "InstallStateUpdatedListener: state: " + state.installStatus()
+                        )
+                    }
+                }
+            }
+        }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        @Nullable data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_APP_UPDATE) {
+            if (resultCode != Activity.RESULT_OK) {
+                Log.e("Android", "onActivityResult: app download failed")
+            }
+        }
+    }
+
+    //PopUpView
+    private fun popupSnackbarForCompleteUpdate(): Unit {
+        val snackbar: Snackbar = Snackbar.make(
+            findViewById(R.id.drawer_layout),
+            "App has been updated",
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snackbar.setAction("Install") { view ->
+            appUpdateManager?.completeUpdate()
+        }
+        snackbar.setActionTextColor(resources.getColor(R.color.wildColor))
+        snackbar.show()
+    }
+
+
+    //States
+
+    override fun onResume() {
+        super.onResume()
+        Handler().postDelayed({ updateApp() }, 1000)
+    }
+
+
 }
