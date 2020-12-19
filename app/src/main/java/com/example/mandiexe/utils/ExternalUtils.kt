d012b8db46f94387c5249f9007640b9b70a23581
Kavya-24 <Kavya_1901cs30@iitp.ac.in>
2020-12-19 18:01:53 +0530

    Add external utils 1. Check network state 2. Retrofit throwable states 3. Epoch conversion to standard time

---
+++ app/src/main/java/com/example/mandiexe/utils/ExternalUtils.kt
@@ -0,0 +1,82 @@
+package com.example.mandiexe.utils
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import com.example.mandiexe.R
+import retrofit2.HttpException
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeoutException
+
+class ExternalUtils {
+
+    //##Access network state
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun checkIsNetworkConnected(context: Context): Boolean {
+
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+        return networkCapabilities != null &&
+                networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+
+    }
+
+    fun returnStateMessageForThrowable(throwable: Throwable): String {
+        var message: String = ""
+
+
+        when (throwable) {
+            is IOException -> {
+                message = ApplicationUtils.getContext()
+                    .getString(R.string.error_please_check_internet)
+            }
+            is TimeoutException -> {
+                message = ApplicationUtils.getContext()
+                    .getString(R.string.error_request_timed_out)
+            }
+
+            //##Str for it
+            is HttpException -> {
+                val httpException = throwable
+                val response = httpException.response()?.errorBody()?.string()
+
+                message = response!!
+            }
+            else -> {
+                message = ApplicationUtils.getContext()
+                    .getString(R.string.error_something_went_wrong)
+            }
+        }
+
+        return message
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun convertTimeToEpoch(timestamp: String): String {
+        val calendar = Calendar.getInstance()
+        val timezone = TimeZone.getTimeZone("UTC")
+        val timeDestinationZone = calendar.timeZone
+        val sourceFormat =
+            SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        val destFormat =
+            SimpleDateFormat("dd-MMM-yyyy")
+
+        sourceFormat.timeZone = timezone
+        val convertedDate = sourceFormat.parse(timestamp)!!
+        destFormat.timeZone = timeDestinationZone
+        Log.e("Timezone", timezone.toString() + timeDestinationZone.toString())
+        return destFormat.format(convertedDate)
+
+
+    }
+
+
+}
