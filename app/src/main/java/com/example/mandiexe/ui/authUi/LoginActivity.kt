877ea768c601c83625a912d52a213530ae66f0bf
Kavya-24 <Kavya_1901cs30@iitp.ac.in>
2020-12-20 18:23:00 +0530

    1. Add splash screen

---
+++ app/src/main/java/com/example/mandiexe/ui/authUi/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mandiexe.ui.authUi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mandiexe.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/example/mandiexe/ui/authUi/SplashActivity.kt
+++ app/src/main/java/com/example/mandiexe/ui/authUi/SplashActivity.kt
@@ -1,8 +1,11 @@
 package com.example.mandiexe.ui.authUi
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mandiexe.MainActivity
 import com.example.mandiexe.R
 import com.example.mandiexe.utils.auth.PreferenceManager
 
@@ -12,14 +15,65 @@ class SplashActivity : AppCompatActivity() {
     private lateinit var runnable: Runnable
     private var splash_display_time: Long = 500
     private val preferenceManager: PreferenceManager = PreferenceManager()
-
+    private val TAG = SplashActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(
             R.layout.activity_splash
         )
+        handler = Handler()
 
+        /*val preferences =
+            getSharedPreferences(getString(R.string.intro_prefs), Context.MODE_PRIVATE)
+        val firstRun = preferences.getBoolean(getString(R.string.intro_prefs_first_run), true)
+
+        if (firstRun) {
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+
+
+
+
+        }
+        */
+
+
+        val intent = if (preferenceManager.authToken!!.isEmpty()) {
+            Log.e(TAG, "In no access token")
+            Intent(this, LoginActivity::class.java)
+        } else {
+            Log.e(TAG, "In success atoken")
+            Intent(this, MainActivity::class.java)
+
+        }
+
+
+        handler = Handler()
+
+        runnable = Runnable {
+            startActivity(intent)
+            finish()
+        }
+
+        handler.postDelayed(runnable, splash_display_time)
+
+
+
+        handler = Handler()
+
+        runnable = Runnable {
+            startActivity(intent)
+            finish()
+        }
+
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(runnable)
 
     }
 }
