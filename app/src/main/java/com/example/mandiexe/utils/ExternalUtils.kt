71c9ae90fdfd0a5b276bfa14135fd7a0083f93af
Kavya-24 <Kavya_1901cs30@iitp.ac.in>
2021-01-15 13:17:42 +0530

    Add External Utils for textTranslate

--- app/src/main/java/com/example/mandiexe/utils/ExternalUtils.kt
+++ app/src/main/java/com/example/mandiexe/utils/ExternalUtils.kt
@@ -16,12 +16,15 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.example.mandiexe.R
 import com.google.android.material.snackbar.Snackbar
+import com.mannan.translateapi.Language
+import com.mannan.translateapi.TranslateAPI
+import com.mannan.translateapi.TranslateAPI.TranslateListener
 import retrofit2.HttpException
 import java.io.IOException
-import java.net.URLEncoder
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeoutException
+
 
 //import org.apache.http.HttpResponse;
 //import org.apache.http.HttpStatus;
@@ -120,7 +123,7 @@ object ExternalUtils {
 
     }
 
-
+    @SuppressLint("SimpleDateFormat")
     fun reverseToReq(timestamp: String): String {
 
         val calendar = Calendar.getInstance()
@@ -140,21 +143,7 @@ object ExternalUtils {
 
     }
 
-
-//    @RequiresApi(Build.VERSION_CODES.O)
-//    fun convertTimestampToDate(timestamp: String): Date? {
-//
-//        val formatter = DateTimeFormatter.ofPattern(
-//            "dd-MMM-yyyy",
-//            Locale(PreferenceUtil.getLanguageFromPreference()) ?: Locale.ENGLISH
-//        )
-//        val ts = convertTimeToEpoch(timestamp)
-//        val localDate = LocalDate.parse(ts, formatter)
-//
-//        return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant())
-//
-//    }
-
+    @SuppressLint("SimpleDateFormat")
     fun convertLastModified(timestamp: String): String {
 
         val calendar = Calendar.getInstance()
@@ -169,6 +158,32 @@ object ExternalUtils {
         val convertedDate = sourceFormat.parse(timestamp)!!
         destFormat.timeZone = timeDestinationZone
         Log.e("Timezone", timezone.toString() + timeDestinationZone.toString())
+        return destFormat.format(convertedDate)
+
+
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun convertDateToReqForm(value: String): String {
+
+        val calendar = Calendar.getInstance()
+        val timezone = TimeZone.getTimeZone("UTC")
+        val timeDestinationZone = calendar.timeZone
+
+        val sourceFormat =
+            SimpleDateFormat("dd/MM/yyyy")
+        val destFormat =
+            SimpleDateFormat("MM-dd-yyyy 00:00:00")
+
+        sourceFormat.timeZone = timezone
+        val convertedDate = sourceFormat.parse(value)!!
+        destFormat.timeZone = timeDestinationZone
+        Log.e("Timezone", timezone.toString() + timeDestinationZone.toString())
+
+        Log.e(
+            "ExternalUtil",
+            "Source date " + value + " Dest Date" + destFormat.format(convertedDate)
+        )
         return destFormat.format(convertedDate)
 
 
@@ -193,9 +208,9 @@ object ExternalUtils {
             val dm: DisplayMetrics = resources.displayMetrics
             val config: Configuration = resources.configuration
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                config.setLocale(Locale(languageFromPreference.toLowerCase()))
+                config.setLocale(Locale(languageFromPreference.toLowerCase(Locale.ROOT)))
             } else {
-                config.locale = Locale(languageFromPreference.toLowerCase())
+                config.locale = Locale(languageFromPreference.toLowerCase(Locale.ROOT))
             }
             resources.updateConfiguration(config, dm)
         }
@@ -223,56 +238,56 @@ object ExternalUtils {
         imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
-    fun convertDateToReqForm(value: String): String {
 
-        val calendar = Calendar.getInstance()
-        val timezone = TimeZone.getTimeZone("UTC")
-        val timeDestinationZone = calendar.timeZone
+    fun translateTextToEnglish(mText: String, srcLanguage: String, dstLanguage: String): String? {
 
-        val sourceFormat =
-            SimpleDateFormat("dd/MM/yyyy")
-        val destFormat =
-            SimpleDateFormat("MM-dd-yyyy 00:00:00")
+        val translateAPI = TranslateAPI(
+            srcLanguage,  //Source Language
+            Language.ENGLISH,  //Target Language
+            mText
+        ) //Query Text
 
-        sourceFormat.timeZone = timezone
-        val convertedDate = sourceFormat.parse(value)!!
-        destFormat.timeZone = timeDestinationZone
-        Log.e("Timezone", timezone.toString() + timeDestinationZone.toString())
+        var q = ""
 
-        Log.e(
-            "ExternalUtil",
-            "Source date " + value + " Dest Date" + destFormat.format(convertedDate)
-        )
-        return destFormat.format(convertedDate)
+        translateAPI.setTranslateListener(object : TranslateListener {
+            override fun onSuccess(translatedText: String) {
+                Log.e("EXternal Utils", "In language conversion error " + translatedText)
+                q = translatedText
+            }
 
+            override fun onFailure(ErrorText: String) {
+                Log.e("EXternal Utils", "In language conversion error " + ErrorText)
 
+            }
+        })
+
+        return q
     }
 
-    fun translateText(text: String?, srcLanguage: Locale, dstLanguage: Locale): String? {
 
-        var translated: String? = null
-        try {
+    fun translateTextToDefault(mText: String, srcLanguage: String, dstLanguage: String): String {
 
-            val query: String = URLEncoder.encode(text, "UTF-8")
-            val langpair: String = URLEncoder.encode(
-                srcLanguage.language.toString() + "|" + dstLanguage.language, "UTF-8"
-            )
-            val url =
-                "http://mymemory.translated.net/api/get?q=$query&langpair=$langpair"
+        val translateAPI = TranslateAPI(
+            Language.ENGLISH,  //Source Language
+            dstLanguage,  //Target Language
+            mText
+        ) //Query Text
 
-//            val hc: HttpClient = DefaultHttpClient()
-//            val hg = HttpGet(url)
-//            val hr: HttpResponse = hc.execute(hg)
-//            if (hr.getStatusLine().getStatusCode() === HttpStatus.SC_OK) {
-//                val response = JSONObject(EntityUtils.toString(hr.getEntity()))
-//                translated = response.getJSONObject("responseData").getString("translatedText")
-//            }
+        var q = ""
 
+        translateAPI.setTranslateListener(object : TranslateListener {
+            override fun onSuccess(translatedText: String) {
+                Log.e("EXternal Utils Default", "In language conversion error " + translatedText)
+                q = translatedText
+            }
 
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return translated
+            override fun onFailure(ErrorText: String) {
+                Log.e("EXternal Utils Default", "In language conversion error " + ErrorText)
+
+            }
+        })
+
+        return q
     }
 
 
